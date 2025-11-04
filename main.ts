aeab4ef867d98b9f056b7f04179e37f61a25a46f
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-11-04 16:38:10 -0500

    fixed code

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Jack McNulty
  * Created on: Oct 2025
- * This program uses a distance sensor
+ * This program uses a distance sensor and neopixels
 */
 
 // variables
@@ -10,14 +10,13 @@ let distanceToObject: number = 0
 let neopixelStrip: neopixel .Strip = null
 
 // setup
-
+basic.clearScreen()
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
-basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // getting distance
@@ -29,21 +28,32 @@ input.onButtonPressed(Button.A, function () {
     )
     basic.clearScreen()
     basic.showNumber(distanceToObject)
+    basic.showString('cm')
     basic.showIcon(IconNames.Happy)
 
     // checking if the distance is more or less than 10cm
-    if (distanceToObject > 10) {
+    if (distanceToObject < 10) {
         basic.clearScreen()
+        basic.showIcon(IconNames.No)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+    } else {
+        basic.showIcon(IconNames.Yes)
         neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
         neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
         neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-        neopixelStrip.show()
-    } else (distanceToObject < 10) 
-        basic.clearScreen()
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
-        neopixelStrip.show()
-    })
+    }
+    neopixelStrip.show()
+    basic.pause(5000)
+    basic.clearScreen()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
